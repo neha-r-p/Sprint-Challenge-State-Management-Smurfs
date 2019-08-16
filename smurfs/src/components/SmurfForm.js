4418b524a32c0930b3d4a9878283c8b1d28bc4a2
Neha Patel <neha.r.p@gmail.com>
2019-08-16 10:40:33 -0700

    form resetting properly

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,20 +1,49 @@
 import React, { useState } from "react";
 
+const submitHandler = event => {
+  console.log("submit button clicked");
+  event.preventDefault();
+  event.target.reset();
+};
+
 const SmurfForm = () => {
+  const [form, setForm] = useState();
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <h3>Create a Smurf!</h3>
       <label>
         Smurf Name:
-        <input type="text" placeholder="name" />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          onChange={event => {
+            setForm(event.target.value);
+          }}
+        />
       </label>
       <label>
         Smurf Age:
-        <input type="text" placeholder="age" />
+        <input
+          type="number"
+          name="age"
+          placeholder="age"
+          onChange={event => {
+            setForm(event.target.value);
+          }}
+        />
       </label>
       <label>
         Smurf Height:
-        <input type="text" placeholder="height" />
+        <input
+          type="text"
+          name="height"
+          placeholder="height (e.g. 5cm)"
+          onChange={event => {
+            setForm(event.target.value);
+          }}
+        />
       </label>
       <button>Add to Village</button>
     </form>
