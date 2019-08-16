7734fa0d4c9b60d97a39614395b49257c8313741
Neha Patel <neha.r.p@gmail.com>
2019-08-16 10:19:05 -0700

    creating form

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SmurfForm = () => {
     return(
-        <div>
-            Form Goes Here
-        </div>
+        <form>
+            <h3>Create a Smurf!</h3>
+            <label>Smurf Name:
+            <input type="text" placeholder="name" />
+            </label>
+            <button>Add to Village</button>
+        </form>
     )
 }
 
