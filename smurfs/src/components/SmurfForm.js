d41cb6e8d9b5601a8c9c140b7bee085892a78c2b
Neha Patel <neha.r.p@gmail.com>
2019-08-16 10:25:40 -0700

    form fields created

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SmurfForm = () => {
-    return(
-        <form>
-            <h3>Create a Smurf!</h3>
-            <label>Smurf Name:
-            <input type="text" placeholder="name" />
-            </label>
-            <button>Add to Village</button>
-        </form>
-    )
-}
+  return (
+    <form>
+      <h3>Create a Smurf!</h3>
+      <label>
+        Smurf Name:
+        <input type="text" placeholder="name" />
+      </label>
+      <label>
+        Smurf Age:
+        <input type="text" placeholder="age" />
+      </label>
+      <label>
+        Smurf Height:
+        <input type="text" placeholder="height" />
+      </label>
+      <button>Add to Village</button>
+    </form>
+  );
+};
 
 export default SmurfForm;
